8a7f656a497c39f5a3f99cf0a62047d8095d0079
Khun <khunly@bstorm.be>
2026-06-11 11:41:17 +0200

    Add initial HTML and JavaScript files for date and pause functionality

---
+++ ex1.js
@@ -0,0 +1,42 @@
+// recupérer la date du jour
+let ajd = new Date();
+// récuperer les heures
+let heures = ajd.getHours();
+// récupérer les minutes
+let minutes = ajd.getMinutes();
+
+const HEURES_PAUSE = 12;
+const MINUTES_PAUSE = 15;
+
+// si heures == HEURES_PAUSE 
+// ET si minutes ENTRE MINUTES_PAUSE et MINUTES PAUSE + 15
+// C'est la pause
+// sinon c'est pas la pause
+
+// et: && ou: ||
+if (heures == HEURES_PAUSE 
+    && minutes >= MINUTES_PAUSE 
+    && minutes <= MINUTES_PAUSE + 15
+) {
+    document.body.innerText = "C'est l'heure de la pause"
+} else {
+    let heure_restantes = HEURES_PAUSE - heures;
+    let minutes_restantes = MINUTES_PAUSE - minutes;
+    if(minutes_restantes < 0) {
+        // recalculer les minutes et les heures si le nombre de minutes est < 0
+        heure_restantes -= 1;
+        minutes_restantes += 60
+    }
+
+    // document.body.innerText = "Il reste " + heure_restantes + "h" +
+    //     minutes_restantes + " avant la pause";
+
+    if(heure_restantes == 0) {
+        document.body.innerText = `Il reste ${minutes_restantes} minutes avant la pause !`
+    } else {
+        // permet de formater les minutes en 9 -> 09 ou 0 -> 00
+        m = minutes_restantes.toString().padStart('2', '0')
+        document.body.innerText = `Il reste ${heure_restantes}h${m} avant la pause !`
+    }
+    
+}
---
+++ main.js
@@ -0,0 +1,28 @@
+// // variables
+
+// // variables scalaires 
+// let maVariable = 'coucou'; // "coucou" `coucou`
+// let nb = 42;
+// let flag = true;
+// let date = new Date()
+
+// // variables composites
+// let array = ['c#', 'js', 'python', 'sql'];
+// let obj = { nom: 'LY', prenom: 'Khun', age: 44 };
+// let bibliotheque = [
+//     { titre: 'La nuit de temps', auteur: 'Barjavel' },
+//     { titre: 'Bilbo le hobbit', auteur: 'Tolkien' },
+// ]
+
+// if(/*condition*/ nb > 55) {
+//     // instructions
+// } else if (/* autre condition */ nb < 12) {
+//     // autres instruction
+// } else {
+//     // encore d'autres instructions
+// }
+
+// // la date du jour
+// let today = new Date();
+// console.log(today.getHours());
+
